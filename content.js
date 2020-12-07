81171f715b3a93a33c72560af3a1201d47db23f7
Patrick Pape <ppape@ejwatercoop.com>
2020-12-07 12:48:44 -0600

    removed message passing

--- content.js
+++ content.js
@@ -1,12 +1,9 @@
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
-  document.addEventListener("contextmenu", function(e) {
-    let copyFrom = document.createElement("textarea");
-    copyFrom.textContent = window.location.href + "#" + e.target.id;
-    document.body.appendChild(copyFrom);
-    copyFrom.select();
-    document.execCommand('copy');
-    copyFrom.blur();
-    document.body.removeChild(copyFrom);
-  })
+document.addEventListener("contextmenu", function(e) {
+  let copyFrom = document.createElement("textarea");
+  copyFrom.textContent = window.location.href + "#" + e.target.id;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  copyFrom.blur();
+  document.body.removeChild(copyFrom);
 });
